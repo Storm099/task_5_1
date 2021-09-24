b25e837f999341dea4b54ffe5de998fa7309ad94
Storm099 <Yorqinbekabrayev1999@gmail.com>
2021-09-24 14:41:33 +0300

    change background color

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.purple,
       body: Container(
         padding: EdgeInsets.only(top: 20),
         child: Row(
